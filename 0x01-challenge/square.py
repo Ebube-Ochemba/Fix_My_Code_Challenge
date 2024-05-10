19b07951e25b71b012bcc9cb32528ae418142c32
Ebube-Ochemba <ebube17@gmail.com>
2024-05-10 01:42:07 +0100

    doc

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
-class square():
+"""square
+define a class Square
+"""
 
-    width = 0
-    height = 0
 
+class Square():
+    """A class Square"""
 
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
@@ -15,14 +17,23 @@ class square():
         return self.width * self.width
 
     def PermiterOfMySquare(self):
+        """ Perimeter of square """
         return (self.width * 2) + (self.width * 2)
 
     def __str__(self):
+        """ Description of square: string representation """
         return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
+
+
+# step 1: class - Square, not square
+# step 2: Add docs where necessary
+# step 3: remove *arg and **kwargs - it's a class not a function*
+# step 4: pycodestyle checks
