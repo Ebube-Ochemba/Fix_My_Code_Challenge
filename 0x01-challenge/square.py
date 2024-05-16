9cf7839dc2bceb3abee84966ff4711853adf1107
Ebube-Ochemba <ebube17@gmail.com>
2024-05-16 02:32:48 +0100

    more tweaks

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -16,9 +16,9 @@ class Square():
 
     Methods
     -------
-        area()
+        area_of_my_square()
             Calculates the area of the square
-        permiter()
+        permiter_of_my_square()
             Calculates the perimeter of the square
         str()
             Prints a string representation of the square
@@ -37,11 +37,11 @@ class Square():
         for key, value in kwargs.items():
             setattr(self, key, value)
 
-    def area(self):
+    def area_of_my_square(self):
         """ Area of the square """
         return self.width * self.width
 
-    def permiter(self):
+    def permiter_of_my_square(self):
         """ Perimeter of square """
         return (self.width * 2) + (self.height * 2)
 
@@ -54,8 +54,8 @@ if __name__ == "__main__":
 
     s = Square(width=12, height=9)
     print(s)
-    print(s.area())
-    print(s.permiter())
+    print(s.area_of_my_square())
+    print(s.permiter_of_my_square())
 
 
 # step 1: class - Square, not square
